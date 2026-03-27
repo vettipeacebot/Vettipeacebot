2fe5e7ed0599e0b139f2d4ab5bd46efd8ffab064
vettipeacebot <mdaflal42@gmail.com>
2026-03-27 12:28:12 +0530

    Update mybot.py

--- mybot.py
+++ mybot.py
@@ -779,9 +779,8 @@ def main():
     app.add_handler(CommandHandler("filters", list_filters))
 
     # 🔥 EVENTS
-       app.add_handler(MessageHandler(filters.ALL & ~filters.COMMAND, filter_all))
-    app.add_handler(MessageHandler(filters.ALL & ~filters.COMMAND, save_group))
-
+app.add_handler(MessageHandler(filters.ALL & ~filters.COMMAND, filter_all))
+app.add_handler(MessageHandler(filters.ALL & ~filters.COMMAND, save_group))
     print("🔥 SECURITY BOT V12 ULTRA RUNNING 🔥")
 
     # 🔥 BACKGROUND TASK
